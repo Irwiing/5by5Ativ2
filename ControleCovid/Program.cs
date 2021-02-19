8be207ee3724bcdc67dc7255385baea86f959a14
VitorFaccio1 <53455640+VitorFaccio1@users.noreply.github.com>
2021-02-19 13:37:53 -0300

    Funcao dados paciente!

--- ControleCovid/Program.cs
+++ ControleCovid/Program.cs
@@ -4,12 +4,84 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ControleCovid
+namespace Projeto_Hospital
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Menu_Fila_Chegada();
+            Console.WriteLine("Pressione qualquer tecla para sair...");
+            Console.ReadKey();
+        }
+
+        static void Menu_Fila_Chegada()
+        {
+            string escolha;
+
+            Console.WriteLine("Informe o que você deseja: ");
+            do
+            {
+                Console.WriteLine("\n1)Cadastrar paciente: " +
+                                  "\n2)Mostrar Fila do atendimento inicial: " +
+                                  "\n3)Procurar Pessoas por ID: " +
+                                  "\n4)Mostrar fila com pacientes com Covid-19" +
+                                  "\n5)Mostrar fila com pacientes sem Covid 19" +
+                                  "\n6)Mostrar Internações" +
+                                  "\n7)Sair do programa!");
+                escolha = Console.ReadLine();
+
+                switch (escolha)
+                {
+                    case "1":
+                        {
+                            Dados_Paciente();
+                            break;
+                        }
+                    case "2":
+                        {
+                            break;
+                        }
+                    case "3":
+                        {
+                            break;
+                        }
+                    case "4":
+                        {
+                            break;
+                        }
+                    case "5":
+                        {
+                            break;
+                        }
+                    case "6":
+                        {
+                            break;
+                        }
+                }
+
+            } while (escolha != "7");
+        }
+
+        static void Dados_Paciente()
+        {
+            string nome, sexo;
+            long cpf;
+            DateTime data_nascimento;
+
+            Console.WriteLine("Informe o nome do paciente: ");
+            nome = Console.ReadLine();
+            Console.WriteLine("Informe o sexo do paciente: ");
+            sexo = Console.ReadLine();
+            Console.WriteLine("Informe o CPF do paciente: ");
+            cpf = long.Parse(Console.ReadLine());
+            Console.WriteLine("Informe a data de nascimento do paciente: ");
+            data_nascimento = DateTime.Parse(Console.ReadLine());
+        }
+
+        static void Agravantes()
+        {
+            string diabetes, hipertensão, obesidade;
         }
     }
 }
